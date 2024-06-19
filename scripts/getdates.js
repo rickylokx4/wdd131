340fdcf054a8c0cd8c87483a379b565dc805ade0
rickylokx4 <rsalazar@byui.edu>
2024-06-19 20:49:57 +0200

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,8 @@
-const date = new Date();
-const year = date.getFullYear();
-const text = document.getElementById('currentyear');
-text.innerHTML = year;
+const today = new Date();
+const year = today.getFullYear();
+const yearText = document.getElementById('currentyear');
+yearText.innerHTML=today.getFullYear();
+
+let LastModif = new Date(document.lastModified);
+const lastModified = document.getElementById('lastModified')
+lastModified.innerHTML="Last Modafied: "+ LastModif
